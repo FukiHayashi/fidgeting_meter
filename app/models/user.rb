6da505a36a33f98080b7ebc0b5f19bb88203a804
Fuki Hayashi <knightf884@yahoo.co.jp>
2021-04-02 07:21:01 +0900

    userモデルの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,18 @@
 class User < ApplicationRecord
+  has_many :measured_fidgets, dependent: :destroy
+  has_many :evaluation_fidgets, dependent: :destroy
+  has_many :api_keys, dependent: :destroy
+
+  has_one :setting, dependent: :destroy
+
+  validates :name, presence: true, length: { maximum:50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum:255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+
+  validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
+  validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
+  validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
+
 end
