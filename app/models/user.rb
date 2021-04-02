2f030b82bb4d52d1f148a6ea1cf3f3ce59308184
Fuki Hayashi <knightf884@yahoo.co.jp>
2021-04-02 22:02:46 +0900

    rubocop対策

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,14 +6,13 @@ class User < ApplicationRecord
 
   has_one :setting, dependent: :destroy
 
-  validates :name, presence: true, length: { maximum:50 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum:255 },
+  validates :name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
+  validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
 
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
-
 end
