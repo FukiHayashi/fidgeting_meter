97c6fde4f18fcb2eeac3eeb776a62f194baba439
Fuki Hayashi <knightf884@yahoo.co.jp>
2021-04-03 00:05:40 +0900

    users actionの作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,47 @@
 class UsersController < ApplicationController
-  def index
+  before_action :set_user, only: %i[show edit update destroy]
+
+  def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
+  end
+
+  def delete
+    @user.destroy!
+
+    redirect_to new_user_path
+  end
+
+  def show; end
+
+  def edit; end
+
+  def update
+    authorize(@user)
+
+    if @user.update(user_params)
+      redirect_to admin_user_path(@user)
+    else
+      render :show
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(policy(:user).permitted_attributes)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
