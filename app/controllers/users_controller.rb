2ededc8038350205228b389d68993ccf6211d393
Fuki Hayashi <knightf884@yahoo.co.jp>
2021-04-03 10:50:05 +0900

    users newページの作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -38,7 +38,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(policy(:user).permitted_attributes)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 
   def set_user
