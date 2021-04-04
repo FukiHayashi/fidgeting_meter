4e123362943e8ed0d70c755d79a197145ebe8ed4
Fuki Hayashi <knightf884@yahoo.co.jp>
2021-04-04 14:43:49 +0900

    login機能の作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'authentication/new'
-  get 'authentication/create'
-  get 'authentication/destory'
   root 'users#new'
   resources :users, only: %i[new create destroy edit update show]
+  get 'login', to: 'authentication#new'
+  post 'login', to: 'authentication#create'
+  delete 'logout', to: 'authentication#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
