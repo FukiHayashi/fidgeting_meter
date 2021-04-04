223cb83cfbfb72e3309a158e9a6e8433a53dcafd
Fuki Hayashi <knightf884@yahoo.co.jp>
2021-04-04 14:23:43 +0900

    authentication_controllerの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'authentication/new'
+  get 'authentication/create'
+  get 'authentication/destory'
   root 'users#new'
   resources :users, only: %i[new create destroy edit update show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
