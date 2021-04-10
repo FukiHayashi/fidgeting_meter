c2e4d8dfb6e6e6a96ea9fea242e12114f8e36231
Fuki Hayashi <knightf884@yahoo.co.jp>
2021-04-10 23:08:54 +0900

    frustration_levelをfidget_calorieに変更

--- app/lib/evaluation_fidgets.rb
+++ app/lib/evaluation_fidgets.rb
@@ -1,17 +1,17 @@
 class EvaluationFidgets
-  attr_accessor :evaluate_days, :comprehensive_evaluation, :frustration_levels, :fidget_level_maximums
+  attr_accessor :evaluate_days, :comprehensive_evaluation, :fidget_times, :fidget_level_maximums, :fidget_calories
 
   def initialize(user)
     @evaluate_days = []
-    @frustration_levels = []
+    @fidget_times = []
+    @fidget_calories = []
     @fidget_level_maximums = []
     (6.days.ago.to_datetime..Date.today.to_datetime).each do |day|
-      puts day.to_date
       fidget_levels = user.measured_fidgets.where(detected_at: day.all_day).pluck(:fidget_level)
       measured_times = user.measured_fidgets.where(detected_at: day.all_day).pluck(:measured_time)
       set_evaluate(day, measured_times, fidget_levels)
     end
-    @comprehensive_evaluation = calc_comprehensive_evaluation(@frustration_levels, @fidget_level_maximums)
+    @comprehensive_evaluation = calc_comprehensive_evaluation
   end
 
   private
@@ -19,16 +19,18 @@ class EvaluationFidgets
   def set_evaluate(day, measured_times, fidget_levels)
     @evaluate_days.push(day.to_date)
     if fidget_levels.count.positive? && measured_times.count == fidget_levels.count
-      @frustration_levels.push(calc_frustration_level(fidget_levels, measured_times))
+      @fidget_times.push(measured_times.sum)
       @fidget_level_maximums.push(calc_maximum_fidget_level(fidget_levels))
+      @fidget_calories.push(measured_times.sum / 3_600 * 40_000)
     else
-      @frustration_levels.push(0)
+      @fidget_times.push(0)
       @fidget_level_maximums.push(0)
+      @fidget_calories.push(0)
     end
   end
 
-  def calc_comprehensive_evaluation(frustration_levels, fidget_level_maximums)
-    average(frustration_levels) * fidget_level_maximums.max
+  def calc_comprehensive_evaluation
+    0
   end
 
   def calc_frustration_level(fidget_levels, measured_times)
