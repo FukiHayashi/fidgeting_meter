2cee00887540eadc93d158cab684b95ce11c3b33
Fuki Hayashi <knightf884@yahoo.co.jp>
2021-04-22 23:48:47 +0900

    レイアウトの調整

--- app/controllers/measured_fidgets_controller.rb
+++ app/controllers/measured_fidgets_controller.rb
@@ -1,5 +1,12 @@
 class MeasuredFidgetsController < ApplicationController
+  before_action :set_user, only: %i[index]
   def index
     @measured_fidgets = current_user.measured_fidgets
+  end
+
+  private
+
+  def set_user
+    @user = User.find(current_user.id)
   end
 end
